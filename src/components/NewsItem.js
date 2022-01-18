ec35d7613f3cbbdef38fc49e34796431a7f80fb9
ammar <ammarkhan575@gmail.com>
2022-01-18 13:29:42 +0530

    23 video completed

---
+++ src/components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+    render() {
+        return (
+            <div>
+                this is news item
+                hello world
+            </div>
+        )
+    }
+}
+
+export default Newsitem
