cdbe743bb8fe70d995e46901743eeddf39e59d06
ammar <ammarkhan575@gmail.com>
2022-01-18 20:25:22 +0530

    video 27 completed and error of map undefined resolved

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,12 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     constructor(){
         super();
-        console.log("Hello i am a constructor");
         this.state={
             articles : [],
             loading : true
         }
     }
     async componentDidMount(){
-        console.log("mount");
         let url= "https://newsapi.org/v2/top-headlines?country=us&apiKey=ae906ebf258e46bca389ae678a3c2dfe";
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -21,17 +19,15 @@ export class News extends Component {
 
     }
     render() {
-        console.log("render")
         return (
             <div className="container my-3">
                 <h2>TruthNews | Top Headlines</h2>
                 <div className="row">
-                {this.state.aricles.map(element=>{
-                    return <div key={element.url} className="col-md-4 my-2">
-                        <NewsItem title={element.title?element.url:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:""} newsUrl={element.url?element.url:""}/>
-                    </div>
-                    
-                })}
+                    {this.state.articles.map((element)=>{
+                        return <div key={element.url} className="col-md-4 my-2">
+                            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:""} newsUrl={element.url?element.url:""}/>
+                        </div>
+                    })}
                 </div>
             </div>
         )
