ff92504af319c82601491f1b953d407f0b40cca7
ammar <ammarkhan575@gmail.com>
2022-01-21 20:46:55 +0530

    app class component is converted into function based components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import { 
@@ -10,41 +10,36 @@ import {
   } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize=15;
-  apiKey=process.env.REACT_APP_NEWS_API;
+const App =()=>{
+  let pageSize=15;
+  let apiKey=process.env.REACT_APP_NEWS_API;
 
-  state = {
-    progress: 0
-  }
+  const [progress, setProgress] = useState(10);
 
-  setProgress = (progress) => {
-    this.setState({progress: progress})
-  }
+  
 
-  render() {
     return (
       <div>
         <Router>
         <LoadingBar
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
       />
         <Navbar/>
         <Routes>
-          <Route exact default path="/" element={<News  setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-          <Route exact default path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-          <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
-          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}/>
-          <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={this.pageSize} country="in" category="sports"/>}/>
-          <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={this.pageSize} country="in" category="technology"/>}/>
-          <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize} country="in" category="health"/>}/>
-          <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
+          <Route exact default path="/" element={<News  setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact default path="/general" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country="in" category="science"/>}/>
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey}  key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/>
+          <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey}  key="sports" pageSize={pageSize} country="in" category="sports"/>}/>
+          <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey}  key="technology" pageSize={pageSize} country="in" category="technology"/>}/>
+          <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey}  key="health" pageSize={pageSize} country="in" category="health"/>}/>
+          <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country="in" category="science"/>}/>
           
         </Routes>
         </Router>
       </div>
-    )
-  }
+    );
 }
 
+export default App;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,4 +1,3 @@
-import { getByTitle } from "@testing-library/react";
 import React from "react";
 
 const Newsitem =(props)=>{
@@ -7,7 +6,7 @@ const Newsitem =(props)=>{
       <div>
         <div className="card">
               <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: "0" }}>
-              <span class="badge rounded-pill bg-danger">
+              <span className="badge rounded-pill bg-danger">
                 {source}
               </span>
               </div>
